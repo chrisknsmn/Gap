5246286b5a731e3b606fb3a70a30db41fe2158e8
Chris Kinsman <chrisknsmn@gmail.com>
2022-12-16 16:47:12 -0500

    JS
    app.js script update

--- App/app.js
+++ App/app.js
@@ -12,44 +12,70 @@ app.get('/', function(req, res) {
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
-// GAP functions
-function set(val, name, type, device, i) {
-    // val - the gap amount in px
-    // name - class name extension 
-    // type - margin or padding
+var dir = 
+[
+    {
+        title: '',
+        el: ['']
+    },
+    {
+        title: '-t',
+        el: ['-top']
+    },
+    {
+        title: '-b',
+        el: ['-bottom']
+    },
+    {
+        title: '-l',
+        el: ['-left']
+    },
+    {
+        title: '-r',
+        el: ['-right']
+    },
 
-    var imp = '';
-    if(i == true) {
-        imp = '!important';
-    } else {
-        imp = '';
+    {
+        title: '-tb',
+        el: ['-top', '-bottom']
+    },
+    {
+        title: '-lr',
+        el: ['-left', '-right']
     }
 
-    var ext = type.charAt(0);
-    var out = '';
-    out += '.'+device+ext+name+' {'+type+': ' + val + 'px '+imp+';}\n';
-    out += '.'+device+ext+'-t'+name+' {'+type+'-top: ' + val + 'px '+imp+';}\n';
-    out += '.'+device+ext+'-b'+name+' {'+type+'-bottom: ' + val + 'px '+imp+';}\n';
-    out += '.'+device+ext+'-l'+name+' {'+type+'-left: ' + val + 'px '+imp+';}\n';
-    out += '.'+device+ext+'-r'+name+' {'+type+'-right: ' + val + 'px '+imp+';}\n';
-    out += '.'+device+ext+'-tb'+name+' {'+type+'-top: ' + val + 'px '+imp+'; '+type+'-bottom: ' + val + 'px '+imp+';}\n';
-    out += '.'+device+ext+'-lr'+name+' {'+type+'-left: ' + val + 'px '+imp+'; '+type+'-right: ' + val + 'px '+imp+';}\n';
-    out += '\n';
-    return out;
+];
+
+
+
+function setClass(val, title, type, dir, device, important) {
+    //Create single class
+    var r = ''
+    r += '.' + device + type.charAt(0) + dir.title + '-' + title + ' { ';
+    //Looping through miltiple directions
+    for (let i = 0; i < dir.el.length; i++) {
+        r += type + dir.el[i] + ': ' + val + 'px ' + important + '; ';
+    }
+    r += '} \n';
+    return r;
+}
+
+function classGroup(val, title, type, imp) {
+    var r = ''
+    for (let i = 0; i < dir.length; i++) {
+        r += setClass(val, title, type, dir[i], '', imp);
+    }
+    r += '\n';
+    return r;
 }
 
 function print() {
-    var gapHalf = 5;
+    var gapHlf = 5;
     var gap = 15; 
 
-    // var gap2 = gap*2; 
-    // var gap3 = gap*3; 
-    // var gap4 = gap*4; 
-    // var gap5 = gap*5; 
-    // var gap6 = gap*6;
-    // var arr = [gap2,gap3,gap4,gap5,gap6];
-
     var arr = [gap*2,gap*3,gap*4,gap*5,gap*6];
+
+    // qtr hlf g dbl 
 
     var out = "";
     var name = '';
@@ -57,93 +83,40 @@ function print() {
     //ALL MARGINS 1-100
     // for (let i = 1; i < 100; i++) {
     //     name = '-' + String((i)) ;
-    //     out += set(i,name,'margin');
+    //     out += set(i,name,'margin','',true);
     // }
 
-    // GAP MARGINS
-    out += '/*Margin*/\n'
-
-    out += set(0,'-0','margin','', true);
-    out += set(gapHalf,'-gh','margin','', true);
-    out += set(gap,'-g','margin','', true);
-
-    for (let i = 0; i < arr.length; i++) {
-        name = '-g' + String((i+2)) ;
-        out += set(arr[i],name,'margin','', true);
-    }
-
-    out += '@media only screen and (max-width: 640px) {\n\n';
-
-    out += set(0,'-0','margin','', true);
-    out += set(gapHalf,'-gh','margin','', true);
-    out += set(gapHalf,'-g','margin','', true);
-
-    for (let i = 0; i < arr.length; i++) {
-        name = '-g' + String((i+2));
-        if(i<1) {
-            out += set(15,name,'margin','', true);
-        } else {
-            out += set(arr[i-1],name,'margin','', true);
-        }
-    }
-    out += '}\n\n';
-
-    // GAP MARGINS MOBILE
-    out += '/*Margins Mobile*/\n'
-    out += '@media only screen and (max-width: 640px) {\n\n';
-    out += set(0,'-0','margin','mob-', true);
-    out += set(gapHalf,'-gh','margin','mob-', true);
-    out += set(gapHalf,'-g','margin','mob-', true);
-    for (let i = 0; i < arr.length; i++) {
-        name = '-g' + String((i+2));
-        if(i<1) {
-            out += set(15,name,'margin','mob-', true);
-        } else {
-            out += set(arr[i-1],name,'margin','mob-', true);
-        }
-    }
-    out += '}\n\n';
-
-    // GAP PADDING
-    out += '/*Padding*/ \n'
-    out += set(0,'-0','padding','', true);
-    out += set(gapHalf,'-gh','padding','', true);
-    out += set(gap,'-g','padding','', true);
-    for (let i = 0; i < arr.length; i++) {
-        name = '-g' + String((i+2)) ;
-        out += set(arr[i],name,'padding','', true);
-    }
-    out += '@media only screen and (max-width: 640px) {\n\n';
     
-    out += set(0,'-0','padding','', true);
-    out += set(gapHalf,'-gh','padding','', true);
-    out += set(gapHalf,'-g','padding','', true);
+    out += classGroup(gapHlf, ( 'g-hlf' ), 'margin', '!important');
+    out += classGroup(gap, ( 'g' ), 'margin', '!important');
 
     for (let i = 0; i < arr.length; i++) {
-        name = '-g' + String((i+2)) ;
-        if(i<1) {
-            out += set(15,name,'padding','', true);
-        } else {
-            out += set(arr[i-1],name,'padding','', true);
-        }
+        out += classGroup(arr[i], ( 'g' + (i+2)), 'margin', '!important');
     }
-    out += '}\n';
+
+    // for (let i = 0; i <= 100; i++) {
+    //     out += classGroup(i, i, 'margin', '!important');
+    //     out += '\n';
+    // }
+
+
 
     // GAP PADDING MOBILE
-    out += '/*Padding Mobile*/\n'
-    out += '@media only screen and (max-width: 640px) {\n\n';
-    out += set(0,'-0','padding','mob-', true);
-    out += set(gapHalf,'-gh','padding','mob-', true);
-    out += set(gapHalf,'-g','padding','mob-', true);
-    for (let i = 0; i < arr.length; i++) {
-        name = '-g' + String((i+2)) ;
-        if(i<1) {
-            out += set(15,name,'padding','mob-', true);
-        } else {
-            out += set(arr[i-1],name,'padding','mob-', true);
-        }
-    }
-    out += '}\n';
+    // out += '/*Padding Mobile*/\n'
+    // out += '@media only screen and (max-width: 640px) {\n\n';
+
+    // out += set(0,'-0','padding','mob-', true);
+    // out += set(gapHalf,'-gh','padding','mob-', true);
+    // out += set(gapHalf,'-g','padding','mob-', true);
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     name = '-g' + String((i+2)) ;
+    //     if(i<1) {
+    //         out += set(15,name,'padding','mob-', true);
+    //     } else {
+    //         out += set(arr[i-1],name,'padding','mob-', true);
+    //     }
+    // }
 
     return out;
 }
